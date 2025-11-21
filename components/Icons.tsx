af2a7db8a35239f16b66a9ced629e2e2c084e14c
thanhvimes <54128105+thanhvimes@users.noreply.github.com>
2025-11-21 11:23:26 +0700

    feat(icons): Add ExclamationCircleIcon component
    Introduces a new SVG icon component, ExclamationCircleIcon, for use in UI elements that require visual indication of errors or warnings.

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -411,3 +411,9 @@ export const ActivityIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => 
     <path strokeLinecap="round" strokeLinejoin="round" d="M13 10V3L4 14h7v7l9-11h-7z" />
   </svg>
 );
+
+export const ExclamationCircleIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg {...props} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+  </svg>
+);
